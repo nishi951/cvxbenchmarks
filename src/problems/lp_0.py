30b6862992484b23cf1c587b3f5c53e4e5e31ba8
Mark <nishimuramarky@yahoo.com>
2016-06-23 16:27:50 -0700

    Fixed generation of LP problems in lp_0.

--- src/problems/lp_0.py
+++ src/problems/lp_0.py
@@ -1,20 +1,30 @@
 # lp_0
-# http://foges.github.io/pogs/egs/linear-program.html
 # minimize c^T x
 # subject to Ax = b and x >= 0
+
+# Lagrangian:
+# c^T x - Lambda^T x + mu^T (Ax - b)
+# For feasible x, Ax - b = 0
+# Then, KKT conditions imply Lambda^T x = 0 (complementary slackness)
+# Finally, we must have c = Lambda - A.T.dot(mu), choosing mu randomly
+#
 
 from cvxpy import *
 import numpy as np
 
-m = 20
-n = 10
+m = 10
+n = 20
 np.random.seed(1)
 
-c = np.random.rand(n)
 A = np.random.rand(m, n)
-x_hat = np.random.rand(n)
-b = A.dot(x_hat)
 
+y = (np.random.rand(n) - 0.5)
+x_hat = -np.select([y < 0], [y])
+Lambda = np.select([y >= 0], [y]) # x_hat^T Lambda = 0
+
+mu = np.random.rand(m)
+c = Lambda - A.T.dot(mu)
+b = A.dot(x_hat)
 x = Variable(n)
 
 objective = Minimize(c*x)
