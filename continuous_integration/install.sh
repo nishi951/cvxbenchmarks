cd6e4fe163723a96372fa001290cf65e79acb34d
Mark <nishimuramarky@yahoo.com>
2017-08-09 16:40:41 -0700

    Updated install.sh, .yml to use conda environment to install scientific python stuff.

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -6,97 +6,37 @@
 
 set -e
 
-sudo apt-get update -qq
-if [[ "$INSTALL_ATLAS" == "true" ]]; then
-    #sudo apt-get install -qq libatlas-base-dev
-    sudo apt-get install -qq libatlas-base-dev gfortran
-    export ATLAS="/usr/lib/atlas-base/libatlas.so"
-fi
+# Deactivate travis virtualenv
+deactivate
 
-if [[ "$DISTRIB" == "conda" ]]; then
-    # Deactivate the travis-provided virtual environment and setup a
-    # conda-based environment instead
-    deactivate
+# Install Miniconda
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+    -O miniconda.sh
+chmod +x miniconda.sh && ./miniconda.sh -b
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
 
-    # Use the miniconda installer for faster download / install of conda
-    # itself
-    wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
-        -O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b
-    export PATH=/home/travis/miniconda/bin:$PATH
-    conda update --yes conda
+# Install scipy/numpy with mkl bindings
+conda create -n testenv --yes python=$PYTHON_VERSION mkl pip  \
+      numpy scipy ecos
 
-    # Configure the conda environment and put it in the path using the
-    # provided versions
-    if [[ "$INSTALL_GLPK" == "true" ]]; then
-        conda create -n testenv --yes python=$PYTHON_VERSION nomkl pip nose \
-              numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION ecos
-    else
-        conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
-              numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION ecos
-        conda install -c cvxgrp --yes scs multiprocess cvxcanon
-    fi
-    source activate testenv
-    # pip install flake8
+source activate testenv
+conda install -c cvxgrp --yes scs multiprocess cvxcanon
+# Reinstall numpy to activate mkl
+# https://github.com/ContinuumIO/anaconda-issues/issues/720
+conda install -f numpy
 
-    # if [[ "$INSTALL_GLPK" == "true" ]]; then
 
-    #     wget http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.5.3.tar.gz
-    #     tar -xf SuiteSparse-4.5.3.tar.gz
-    #     export CVXOPT_SUITESPARSE_SRC_DIR=$(pwd)/SuiteSparse
 
-    #     # Install GLPK.
-    #     wget http://ftp.gnu.org/gnu/glpk/glpk-4.60.tar.gz
-    #     tar -zxvf glpk-4.60.tar.gz
-    #     cd glpk-4.60
-    #     sudo ./configure
-    #     sudo make
-    #     sudo make check
-    #     sudo make install
-    #     sudo ldconfig
-    #     cd ..
-    #     # Install CVXOPT with GLPK bindings.
-    #     CVXOPT_BUILD_GLPK=1 CVXOPT_GLPK_LIB_DIR=/usr/local/lib CVXOPT_GLPK_INC_DIR=/usr/local/include pip install cvxopt
+# if [[ "$DISTRIB" == "ubuntu" ]]; then
+#     sudo apt-get update -qq
+#     # Use standard ubuntu packages in their default version
+#     sudo apt-get install -qq python-pip python-scipy python-numpy
+# fi
 
-    #     # Install CBC
-    #     oldpath="$PWD"
-    #     cd /home/travis
-    #     wget http://www.coin-or.org/download/source/Cbc/Cbc-2.9.7.tgz
-    #     tar -zxvf Cbc-2.9.7.tgz
-    #     cd Cbc-2.9.7
-    #     sudo ./configure
-    #     sudo make
-    #     sudo make install
-    #     cd ..
+# if [[ "$COVERAGE" == "true" ]]; then
+#     pip install coverage coveralls
+# fi
 
-    #     export COIN_INSTALL_DIR=/home/travis/Cbc-2.9.7
-    #     export LD_LIBRARY_PATH=/home/travis/Cbc-2.9.7/lib:$LD_LIBRARY_PATH
-
-    #     # sudo apt-get install coinor-libcbc-dev coinor-libcbc0 coinor-libcbc-doc
-
-    #     # Install cyLP -> which is needed for CBC-interface
-    #     git clone -b py3 https://github.com/jjhelmus/CyLP.git  # use custom-branch because of py3
-    #     cd CyLP
-    #     python setup.py install
-    #     cd ..
-
-    #     cd "$oldpath"
-    # fi
-
-    if [[ "$INSTALL_MKL" == "true" ]]; then
-        # Make sure that MKL is used
-        conda install --yes mkl
-    else
-        # Make sure that MKL is not used
-        conda remove --yes --features mkl || echo "MKL not installed"
-    fi
-
-elif [[ "$DISTRIB" == "ubuntu" ]]; then
-    sudo apt-get update -qq
-    # Use standard ubuntu packages in their default version
-    sudo apt-get install -qq python-pip python-scipy python-numpy
-fi
-
-if [[ "$COVERAGE" == "true" ]]; then
-    pip install coverage coveralls
-fi
+# Install cvxbenchmarks
+python setup.py install
